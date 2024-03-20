fa435eec0c26af8755b47ac0572ee5a0ccf1af46
Dhouibi Marwa <abdellaouimarwa78@gmail.com>
2024-03-20 22:41:13 +0100

    Suite TP 5/6

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,11 +4,18 @@ import { createRouter, createWebHistory } from "vue-router";
 import { routes } from "./routes";
 import "@fortawesome/fontawesome-free/css/all.css";
 import 'bootstrap/dist/css/bootstrap.min.css'
+
+import PrimeVue from 'primevue/config';
+import 'primevue/resources/themes/lara-light-green/theme.css'
+import store from "./store" 
+
 import App from './App.vue'
 let app = createApp(App)
 const router = createRouter({
-history: createWebHistory(),
-routes: routes,
+     history: createWebHistory(),
+     routes: routes,
 })
 app.use(router);
+app.use(PrimeVue);
+app.use(store)
 app.mount("#app")
--- resources/js/routes.js
+++ resources/js/routes.js
@@ -3,31 +3,69 @@ import Viewcategorie from "./components/categories/Viewcategorie.vue"
 import Addcategorie from "./components/categories/Addcategorie.vue"
 import Addaticle from "./components/articles/Addarticle.vue"
 import editarticle from "./components/articles/editarticle.vue"
+import Viewarticletable from "./components/articles/Viewarticletable.vue"
+import Addarticletable from "./components/articles/Addarticletable.vue"
+import Editarticletable from "./components/articles/Editarticletable.vue"
+import Cardarticle from "./components/articles/Cardarticle.vue"
+import HomeCart from './components/cart/HomeCart.vue';
+import Cart from './components/cart/Cart.vue';
 
-export const routes=[
-{
-name:"Viewcategorie",
-path:"/listcat",
-component:Viewcategorie
-},
-{
-name:"Addcategorie",
-path:"/addcategorie",
-component:Addcategorie
-},
-{
-name:"Viewarticles",
-path:"/listart",
-component:Viewarticles
-},
-{
-name:"Addarticle",
-path:"/addarticle",
-component:Addaticle
-},
-{
-name:"editarticle",
-path:"/editarticle/:id",
-component:editarticle
-    }
+
+export const routes = [
+   {
+      name: "Viewcategorie",
+      path: "/listcat",
+      component: Viewcategorie
+   },
+   {
+      name: "Addcategorie",
+      path: "/addcategorie",
+      component: Addcategorie
+   },
+   {
+      name: "Viewarticles",
+      path: "/listart",
+      component: Viewarticles
+   },
+   {
+      name: "Addarticle",
+      path: "/addarticle",
+      component: Addaticle
+   },
+   {
+      name: "editarticle",
+      path: "/editarticle/:id",
+      component: editarticle
+   },
+
+   {
+      name: "Viewarticletable",
+      path: "/articletable",
+      component: Viewarticletable
+   },
+   {
+      name: "Addarticletable",
+      path: "/addarticletable",
+      component: Addarticletable
+   },
+   {
+      name: "Editarticletable",
+      path: "/articletable/edit/:id",
+      component: Editarticletable
+   },
+   {
+      name: "Cardarticle",
+      path: "/cardarticle",
+      component: Cardarticle
+   },
+   {
+      path: '/shopping',
+      name: 'HomeCart',
+      component: HomeCart
+   },
+   {
+      path:'/cart',
+      name:'Cart',
+      component:Cart
+      }
 ]
